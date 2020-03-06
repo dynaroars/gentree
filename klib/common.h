c51d13707b3769c45ec683d181e98de824400006
KH <kh@kh>
2020-03-05 18:15:58 -0600

    initial commit

---
+++ klib/common.h
@@ -0,0 +1,14 @@
+//
+// Created by KH on 3/5/2020.
+//
+
+#ifndef IGEN4_COMMON_H
+#define IGEN4_COMMON_H
+
+namespace igen {
+
+
+
+}
+
+#endif //IGEN4_COMMON_H
---
+++ klib/log.h
@@ -0,0 +1,16 @@
+//
+// Created by KH on 3/5/2020.
+//
+
+#ifndef IGEN4_LOG_H
+#define IGEN4_LOG_H
+
+#include "common.h"
+
+namespace igen {
+
+
+
+}
+
+#endif //IGEN4_LOG_H
