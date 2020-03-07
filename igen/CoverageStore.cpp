7808f4ac3205963dc96ea17f3652325e1f14a798
KH <kh@kh>
2020-03-07 16:49:41 -0600

    c 13

--- igen/CoverageStore.cpp
+++ igen/CoverageStore.cpp
@@ -4,10 +4,47 @@
 
 #include "CoverageStore.h"
 
+#include <boost/container/flat_set.hpp>
+
 namespace igen {
 
 
 CoverageStore::CoverageStore(igen::PMutContext ctx) : Object(move(ctx)) {
+}
+
+void CoverageStore::register_loc(const PMutLocation &loc) {
+    loc->set_id(n_locs());
+    locs_.emplace_back(loc);
+    clocs_.emplace_back(loc);
+}
+
+void CoverageStore::register_config(const PMutConfig &config) {
+    config->set_id(n_configs());
+    configs_.emplace_back(config);
+    cconfigs_.emplace_back(config);
+}
+
+int CoverageStore::register_cov(const PMutConfig &config, const set<str> &loc_names) {
+    int n_new_locations = 0;
+    if (config->id() == -1) register_config(config);
+    for (const str &n : loc_names) {
+        PMutLocation loc = this->loc_mut(n);
+        if (loc == nullptr) {
+            loc = new Location(ctx(), n);
+            register_loc(loc);
+            n_new_locations++;
+        }
+        link(config, loc);
+    }
+    return n_new_locations;
+}
+
+void CoverageStore::link(const PMutConfig &config, const PMutLocation &loc) {
+    int conf_id = config->id(), loc_id = loc->id();
+    CHECK(conf_id != -1 && loc_id != -1);
+    DCHECK(0 <= conf_id && conf_id < n_configs() && 0 <= loc_id && loc_id < n_locs());
+    config->cov_loc_mut_ids().push_back(loc_id);
+    loc->cov_by_mut_ids().push_back(conf_id);
 }
 
 void intrusive_ptr_release(CoverageStore *d) { intrusive_ptr_add_ref(d); }
--- igen/CoverageStore.h
+++ igen/CoverageStore.h
@@ -17,7 +17,32 @@ public:
 
     const PLocation &loc(int id) const { return clocs_.at(id); }
 
+    const PMutLocation &loc_mut(int id) const { return locs_.at(id); }
+
+    PLocation loc(const str &name) const {
+        auto it = map_name_loc_.find(name);
+        return (it == map_name_loc_.end()) ? nullptr : it->second;
+    }
+
+    PMutLocation loc_mut(const str &name) const {
+        auto it = map_name_loc_.find(name);
+        return (it == map_name_loc_.end()) ? nullptr : it->second;
+    }
+
     const PConfig &config(int id) const { return cconfigs_.at(id); }
+
+    int n_locs() const { return int(locs_.size()); }
+
+    int n_configs() const { return int(configs_.size()); }
+
+    void register_loc(const PMutLocation &loc);
+
+    void register_config(const PMutConfig &config);
+
+    // Return no. of new Locations
+    int register_cov(const PMutConfig &config, const set<str> &loc_names);
+
+    void link(const PMutConfig &config, const PMutLocation &loc);
 
 private:
     vec<PMutLocation> locs_;
@@ -25,6 +50,8 @@ private:
 
     vec<PMutConfig> configs_;
     vec<PConfig> cconfigs_;
+
+    map<str, PMutLocation> map_name_loc_;
 };
 
 using PCoverageStore = ptr<const CoverageStore>;
