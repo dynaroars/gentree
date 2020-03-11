0ac92d4d08e2f76021df8f41cf15d81d219fe3ad
KH <kh@kh>
2020-03-10 23:12:17 -0500

    add stream hash

---
+++ klib/StreamHash.h
@@ -0,0 +1,62 @@
+//
+// Created by KH on 3/10/2020.
+//
+
+#ifndef IGEN4_STREAMHASH_H
+#define IGEN4_STREAMHASH_H
+
+#include <utility>
+
+#include "hash.h"
+
+namespace igen {
+
+class StreamHash {
+public:
+    StreamHash(hash128_t IV, uint64_t counter) : KEY(std::move(IV)), counter(counter) {}
+
+    StreamHash() : StreamHash({0xdbafd606665ff759ULL, 0x6d2636fbaebfa7caULL}, 0x4483d4518cf4b479ULL) {}
+
+    void add(int val) {
+        for (int i = 0; i < ROUNDS; i++) {
+            R(KEY.first, KEY.second, (counter << 16u) ^ uint32_t(val));
+            R(VAL.first, VAL.second, KEY.second);
+            counter++;
+        }
+    }
+
+    [[nodiscard]] hash128_t digest() const {
+        hash128_t ret = VAL, tkey = KEY;
+        uint64_t X = ROL(counter, 16);
+        for (int i = 0; i < FINAL_ROUNDS; i++) {
+            R(tkey.first, tkey.second, X ^ uint64_t(i));
+            R(ret.first, ret.second, tkey.second);
+        }
+        return ret;
+    }
+
+private:
+    static constexpr int ROUNDS = 2;
+    static constexpr int FINAL_ROUNDS = 2;
+
+    static inline uint64_t ROR(const uint64_t x, const uint64_t r) {
+        return (x >> r) | (x << (64 - r));
+    }
+
+    static inline uint64_t ROL(const uint64_t x, const uint64_t r) {
+        return (x << r) | (x >> (64 - r));
+    }
+
+    static inline void R(uint64_t &x, uint64_t &y, const uint64_t k) {
+        x = ROR(x, 8), x += y, x ^= k, y = ROL(y, 3), y ^= x;
+    }
+
+private:
+    hash128_t KEY;
+    hash128_t VAL;
+    uint64_t counter;
+};
+
+}
+
+#endif //IGEN4_STREAMHASH_H
